cd9acb963e0e5079ccd3b3cbd8b71240d2d98101
aistudios-in <aistudiosdotin@gmail.com>
2023-04-18 00:09:59 +0530

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react"; 
+import { useContext, useState } from "react"; 
+
 import "./login.scss"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import "../../assets/css/black-dashboard.css"
+import "../../assets/css/nucleo-icons.css"
+
+import { signInWithEmailAndPassword } from "firebase/auth";
 //import { auth } from "../../firebase";
 import auth from "../../firebase";
 import { Password } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import primarycard from '../../assets/img/card-primary.png';
 
 const Login = () => {
 
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const {dispatch} = useContext(AuthContext);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -17,11 +28,13 @@ const Login = () => {
     console.log(email);
     console.log(password);
 
-    createUserWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
           //Signed in
           const user = userCredential.user;
+          dispatch({type:"LOGIN", payload:user});
           console.log(user);
+          navigate('/');
       })
       .catch((error) => {
         setError(true);
@@ -30,14 +43,151 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)}/>
-        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-        {error && <span>Wrong email or password !</span>}
-      </form>
+    <div className="login-page">
+      <nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent fixed-top">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle d-inline">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="javascript:void(0)">Login to Ciellie Portal</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a href="../dashboard.html" class="nav-link text-primary">
+                  <i class="tim-icons icon-minimal-left"></i> Back to Dashboard
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a href="register.html" class="nav-link">
+                  <i class="tim-icons icon-laptop"></i> Register
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a href="login.html" class="nav-link">
+                  <i class="tim-icons icon-single-02"></i> Login
+                </a>
+              </li>
+              <li class="nav-item  active ">
+                <a href="pricing.html" class="nav-link">
+                  <i class="tim-icons icon-coins"></i> Pricing
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a href="lock.html" class="nav-link">
+                  <i class="tim-icons icon-lock-circle"></i> Lock
+                </a>
+              </li>
+            </ul>
+        </div>
+          
+        </div>
+      </nav>
+      <div class="modal modal-search fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="SEARCH" />
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <i class="tim-icons icon-simple-remove"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper wrapper-full-page ">
+        <div class="full-page login-page ">
+          <div class="content">
+            <div class="container">
+              <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+                <form onSubmit={handleLogin}>
+                  <div class="card card-login card-white">
+                    <div class="card-header">
+                      <img src={primarycard} alt="" />
+                      <h1 class="card-title">Log in</h1>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group">
+                          <div class="input-group-prepend">
+                            <div class="input-group-text">
+                              <i class="tim-icons icon-email-85"></i>
+                            </div>
+                          </div>
+                          <input type="email" class="form-control" placeholder="email" onChange={e=>setEmail(e.target.value)}/>
+                        </div>
+                        <div class="input-group">
+                          <div class="input-group-prepend">
+                            <div class="input-group-text">
+                              <i class="tim-icons icon-lock-circle"></i>
+                            </div>
+                          </div>
+                          <input type="password" class="form-control" placeholder="password" onChange={e=>setPassword(e.target.value)} />
+                        </div>
+                        <div class="card-footer">
+                          <button class="btn btn-primary btn-lg btn-block mb-3" type="submit">Get Started</button>
+                          {error && <span>Wrong email or password !</span>}
+                        </div>
+
+                        <div class="pull-left">
+                          <h6>
+                            <a href="..\..\examples\pages\register.html" class="link footer-link">Create Account</a>
+                          </h6>
+                        </div>
+                        <div class="pull-right">
+                          <h6>
+                            <a href="../../examples/pages/pricing.html" class="link footer-link">Need Help?</a>
+                          </h6>
+                        </div>
+
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
+
+    <footer class="footer">
+        <div class="container-fluid">
+          <ul class="nav">
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                Creative Tim
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                About Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                Blog
+              </a>
+            </li>
+          </ul>
+          <div class="copyright">
+            ©
+            <script>
+              document.write(new Date().getFullYear())
+            </script> made with <i class="tim-icons icon-heart-2"></i> by
+            <a href="javascript:void(0)" target="_blank">Creative Tim</a> for a better web.
+          </div>
+        </div>
+      </footer>
+
+  </div>
   )
 }
 
